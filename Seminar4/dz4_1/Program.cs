b8911508291345d014f9297e15520666e041619b
Andrey <sorang2010@yandex.ru>
2022-12-07 12:34:21 +0300

    DZ from seminar 4

---
+++ Seminar4/dz4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// НЕ ИСПОЛЬЗОВАТЬ MATH.POW
+int numberUP(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= a; // result = result * i
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 в степень которого возвести число 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат: {numberUP(a, n)}");
---
+++ Seminar4/dz4_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.Write("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+
+int[] a1 = new int[]{};
+
+while (n != 0)
+{
+    int count_n = n % 10;
+    a1 = a1.Append(count_n).ToArray();
+    // Console.WriteLine(a1);
+    int x = n - count_n;
+    n = x / 10;
+}
+
+int l_array = a1.Length;
+int sum_n = 0;
+
+for (int j = 0; j < l_array; j++)
+{
+    sum_n = sum_n + a1[j];
+}
+Console.WriteLine(sum_n);
